c070ec30fcef3729c194ec766e2ce9738b6cf651
Gilson Reis <gilsonc.reis@gmail.com>
2019-12-04 17:55:01 -0300

    feat(Product): Add slug field

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -20,8 +20,11 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(Product::class, function (Faker $faker) {
+    $title = substr($faker->sentence(), 0, 64);
+    $slug = Str::slug($title);
     return [
-        'title' => substr($faker->sentence(), 0, 64),
+        'title' => $title,
+        'slug' => $slug,
         'description' =>  $faker->text(500),
         'price' => $faker->numberBetween(30, 9999),
         'category_id' => $faker->numberBetween(1, 5),
--- database/migrations/2019_12_03_063815_create_products_table.php
+++ database/migrations/2019_12_03_063815_create_products_table.php
@@ -15,7 +15,8 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title', 64);
+            $table->string('title', 64)->unique();
+            $table->string('slug', 64)->unique();
             $table->text('description');
             $table->decimal('price', 10, 2);
             $table->string('photo', 120)->nullable();
